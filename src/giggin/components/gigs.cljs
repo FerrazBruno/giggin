08f433352a4ec77adfeb7a4a4cc2550c7bdbd353
Bruno Ferraz <brunohenriqueferraz94@gmail.com>
2026-01-19 18:30:30 -0300

    Refatorando o componente gigs

--- src/giggin/components/gigs.cljs
+++ src/giggin/components/gigs.cljs
@@ -5,23 +5,17 @@
 (defn gigs
   []
   [:main
-   [:div {:class "gigs"}
-    (map
-      (fn [gig]
-        [:div {:class "gig"
-               :key (:id gig)}
-         [:img {:class "gig__artwork"
-                :src (:img gig)
-                :alt (:title gig)}]
-         [:div {:class "gig__body"}
-          [:div {:class "gig__title"}
-           [:div {:class "btn btn--primary float--right tooltip"
-                  :data-tooltip "Add to order"}
-            [:i {:class "icon icon--plus"}
-             "title"]]
-           [:p {:class "gig__price"}
-            (:price gig)]
-           [:p {:class "gig_description"}
-            (:desc gig)]]]])
-      (vals @state/gigs))]])
+   [:div.gigs
+    (for [{:keys [id title desc img price]} (vals @state/gigs)]
+      [:div.gig {:key id}
+       [:img.gig__artwork {:src img :alt title}]
+       [:div.gig__boddy
+        [:div.gig__title
+         [:div.btn.btn--primary.float--right.tooltip
+          {:data-tooltip "Add to order"}}
+          [:i.icon.icon--plus "title"]]
+         [:p.gig__price
+          price]
+         [:p.gig_description
+          desc]]]])]])
 
